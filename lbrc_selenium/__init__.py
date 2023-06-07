922082e0be681f3cb9aa47cb81a0e7559534fb47
Richard Bramley <rabramley@gmail.com>
2023-06-07 15:39:01 +0100

    Copy in REDCap testing working version

--- lbrc_selenium/__init__.py
+++ lbrc_selenium/__init__.py
@@ -1,316 +1,107 @@
-from contextlib import contextmanager
-import os
-import zipfile
-import re
-import smtplib
 import logging
-import base64
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
-from urllib.parse import urljoin
-from pathlib import Path
-from email.mime.multipart import MIMEMultipart
-from email.mime.base import MIMEBase
-from email.encoders import encode_base64
-from email.mime.text import MIMEText
-from selenium.webdriver.support.ui import WebDriverWait
-
-
-RE_REMOVE_HTML_TAGS = re.compile('<.*?>')
-
-@contextmanager
-def get_selenium(base_url):
-    args = dict(
-        download_directory=os.environ["LBRC_SELENIUM_DOWNLOAD_DIRECTORY"],
-        base_url=base_url,
-        implicit_wait_time=float(os.environ.get("LBRC_SELENIUM_IMPLICIT_WAIT_TIME", 1.0)),
-        click_wait_time=float(os.environ.get("LBRC_SELENIUM_CLICK_WAIT_TIME", 1)),
-        download_wait_time=float(os.environ.get("LBRC_SELENIUM_DOWNLOAD_WAIT_TIME", 30)),
-        page_wait_time=float(os.environ.get("LBRC_SELENIUM_PAGE_WAIT_TIME", 5))
-    )
-
-    if os.environ.get("LBRC_SELENIUM_HOST", None):
-        helper = SeleniumGridHelper(
-            selenium_host=os.environ["LBRC_SELENIUM_HOST"],
-            selenium_port=os.environ.get("LBRC_SELENIUM_PORT", '4444'),
-            **args,
-        )
-    else:
-        helper = SeleniumLocalHelper(
-            headless=os.environ.get("LBRC_SELENIUM_HEADLESS", False),
-            **args,
-        )
-
-    try:
-        yield helper
-    finally:
-        helper.close()
-
-
-# Selectors
-class Selector:
-    def __init__(self, query, by):
-        self.query = query
-        self.by = by
-
-
-class CssSelector(Selector):
-    def __init__(self, query):
-        super().__init__(query, By.CSS_SELECTOR)
-
-
-class XpathSelector(Selector):
-    def __init__(self, query):
-        super().__init__(query, By.XPATH)
-
-
-class NameSelector(Selector):
-    def __init__(self, query):
-        super().__init__(query, By.NAME)
-
-
-class ClassSelector(Selector):
-    def __init__(self, query):
-        super().__init__(query, By.CLASS_NAME)
-
-
-class IdSelector(Selector):
-    def __init__(self, query):
-        super().__init__(query, By.ID)
+import shutil
+import math
+import os
+import jsonlines
+from werkzeug.utils import secure_filename
 
 
 # Helpers
-class SeleniumHelper:
-    def __init__(
-        self,
-        driver,
-        download_directory,
-        base_url,
-        click_wait_time=0.2,
-        download_wait_time=5,
-        page_wait_time=1,
-    ):
-        self.click_wait_time = click_wait_time
-        self.download_wait_time = download_wait_time
-        self.page_wait_time = page_wait_time
+class Sampler:
+    TYPE_ALL = 'all'
+    TYPE_FIBONACCI = 'fibonacci'
+    TYPE_FIRST = 'first'
 
-        self.driver = driver
+    def __init__(self):
+        self.sampling_type = os.environ.get("SAMPLING_TYPE", self.TYPE_ALL)
 
-        self.base_url = base_url
+    def is_sampling_pick(self, n):
+        is_perfect_square = lambda x: int(math.sqrt(x))**2 == x
 
-        self.download_directory = Path(download_directory)
-        self.download_directory.mkdir(parents=True, exist_ok=True)
-        self._clear_directory(self.download_directory)
+        if self.sampling_type.isnumeric():
+            if (n % 100) < int(self.sampling_type):
+                logging.debug(f'Sampled')
+                return True
+            else:
+                logging.debug(f'unSampled')
+                return False
 
-
-    def unzip_download_directory_contents(self):
-        for zp in self._download_directory.iterdir():
-            with zipfile.ZipFile(zp, "r") as zf:
-                zf.extractall(self._download_directory)
-    
-    def _clear_directory(self, directory):
-        for f in directory.iterdir():
-            f.unlink()
-    
-    def get(self, url):
-        self.driver.get(urljoin(self.base_url, url))
-        self.wait_to_appear(CssSelector('body'))
-
-    def convert_to_relative_url(self, url):
-        if url.startswith(self.base_url):
-            return url[len(self.base_url):]
+        if self.sampling_type == Sampler.TYPE_ALL:
+            return True
+        elif self.sampling_type == Sampler.TYPE_FIRST:
+            return n == 0
         else:
-            return url
-
-    def wait_to_appear(self, selector, element=None, seconds_to_wait=10):
-        return WebDriverWait((element or self.driver), seconds_to_wait).until(lambda x: x.find_element(selector.by, selector.query))
-    
-    def wait_to_disappear(self, selector, element=None, seconds_to_wait=10):
-        return WebDriverWait((element or self.driver), seconds_to_wait).until_not(lambda x: x.find_element(selector.by, selector.query))
-    
-    def get_parent(self, element):
-        return self.get_element(XpathSelector("./.."), element=element)
-    
-    def get_element(self, selector, allow_null=False, element=None):
-        try:
-            return (element or self.driver).find_element(selector.by, selector.query)
-
-        except (NoSuchElementException, TimeoutException) as ex:
-            if not allow_null:
-                raise ex
-
-    def get_elements(self, selector, element=None):
-        return (element or self.driver).find_elements(selector.by, selector.query)
-    
-    def type_in_textbox(self, selector, text, element=None):
-        e = self.get_element(selector, element=element)
-        e.clear()
-        e.send_keys(text)
-        return e
-
-    def click_element(self, selector, element=None):
-        e = self.get_element(selector, element=element)
-        e.click()
-        sleep(self.click_wait_time)
-        return e
-    
-    def click_all(self, selector, element=None):
-        while True:
-            element = self.get_element(selector, allow_null=True, element=element)
-
-            if element is None:
-                break
-            
-            element.click()
-            sleep(self.click_wait_time)
-    
-    def get_text(self, element):
-        result = self.normalise_text(element.text)
-
-        if len(result) == 0:
-            result = self.normalise_text(element.get_attribute("text"))
-
-            if len(result) == 0:
-                result = self.get_innerHtml(element)
-        
-        return result
-    
-    def get_href(self, element):
-        return (element.get_attribute("href") or '').strip()  
-    
-    def get_value(self, element):
-        return self.normalise_text(element.get_attribute("value"))
-    
-    def normalise_text(self, value):
-        with_removed_tags = re.sub(RE_REMOVE_HTML_TAGS, '', (value or ''))
-        return ' '.join(with_removed_tags.split()).strip()
-
-    def get_innerHtml(self, element):
-        return self.normalise_text(self.driver.execute_script("return arguments[0].innerHTML", element))
-    
-    def email_screenshot(self, email_address):
-        msg = MIMEMultipart()
-        msg['Subject'] = 'Your Requested Screenshot from Selenium'
-        msg['To'] = email_address
-        msg['From'] = email_address
-
-        url = self.driver.current_url
-
-        msg.attach(MIMEText(f'Here is the screenshot that you requested of page {url}'))
-
-        part = MIMEBase('image', 'png')
-        part.set_payload(self.driver.get_screenshot_as_png())
-        encode_base64(part)
-
-        part.add_header(       
-            'Content-Disposition',
-            'attachment; filename="screenshot.png"',
-        )
-
-        msg.attach(part)
-
-        s = smtplib.SMTP('smtp.xuhl-tr.nhs.uk')
-        s.send_message(msg)
-        s.quit()
-
-    def download_file(self, filename):
-        # list all the completed remote files (waits for at least one)
-        files = WebDriverWait(self.driver, 30, 1).until(lambda driver: self.get_downloaded_files(driver))
-
-        # get the content of the first file remotely
-        content = self.get_file_content(self.driver, files[0])
-
-        with open(filename, 'wb') as f:
-            f.write(content)
-
-        logging.info("Test test_grid_download_files executed successfully")
-
-    def get_downloaded_files(self, driver):
-        if not driver.current_url.startswith("chrome://downloads"):
-            driver.get("chrome://downloads/")
-
-        return driver.execute_script( \
-            "return  document.querySelector('downloads-manager')  "
-            " .shadowRoot.querySelector('#downloadsList')         "
-            " .items.filter(e => e.state === 'COMPLETE')          "
-            " .map(e => e.filePath || e.file_path || e.fileUrl || e.file_url); ")
-
-    def get_file_content(self, driver, path):
-        try:
-            elem = driver.execute_script( \
-                "var input = window.document.createElement('INPUT'); "
-                "input.setAttribute('type', 'file'); "
-                "input.hidden = true; "
-                "input.onchange = function (e) { e.stopPropagation() }; "
-                "return window.document.documentElement.appendChild(input); " )
-
-            elem._execute('sendKeysToElement', {'value': [ path ], 'text': path})
-
-            result = driver.execute_async_script( \
-                "var input = arguments[0], callback = arguments[1]; "
-                "var reader = new FileReader(); "
-                "reader.onload = function (ev) { callback(reader.result) }; "
-                "reader.onerror = function (ex) { callback(ex.message) }; "
-                "reader.readAsDataURL(input.files[0]); "
-                "input.remove(); "
-                , elem)
-
-            if not result.startswith('data:') :
-                raise Exception("Failed to get file content: %s" % result)
-
-            return base64.b64decode(result[result.find('base64,') + 7:])
-
-        finally:
-            logging.info("get_file_content executed successfully")
+            return is_perfect_square(5*n*n + 4) or is_perfect_square(5*n*n - 4)
 
 
-class SeleniumGridHelper(SeleniumHelper):
-    def __init__(self, download_directory, selenium_host, selenium_port, implicit_wait_time, browser=DesiredCapabilities.CHROME, **kwargs):
+class ItemFileGroup():
+    def __init__(self, helper, output_directory, filename_template, sorted=True):
+        self.helper = helper
+        self.output_directory = output_directory
+        self.filename_template = filename_template
+        self.sorted = sorted
 
-        browser['acceptInsecureCerts'] = True
-        browser['acceptSslCerts'] = True
+    def get_file(self, template_arguments=None):
+        self.output_directory.mkdir(parents=True, exist_ok=True)
+
+        if template_arguments:
+            filename = self.filename_template.format(**template_arguments)
+        else:
+            filename = self.filename_template
+
+        return ItemsFile(self.output_directory, filename, sorted=self.sorted)
+
+    def clean(self):
+        if os.path.exists(self.output_directory):
+            shutil.rmtree(self.output_directory)
 
 
-        driver = webdriver.Remote(
-            command_executor=f'http://{selenium_host}:{selenium_port}/wd/hub',
-            desired_capabilities=browser
-        )
+class ItemsFile():
+    def __init__(self, output_directory, filename, sorted=True):
+        self.output_directory = output_directory
+        self.filename = filename
+        self.items = []
+        self.sorted = sorted
 
-        super().__init__(
-            driver=driver,
-            download_directory=download_directory,
-            **kwargs,
-        )
+    def _export_filename(self):
+        return secure_filename(f'{self.filename}.jsonl')
 
-    def close(self):
-        self.driver.quit()
+    def add_item(self, item):
+        if item not in self.items:
+            self.items.append(item)
+
+    def exists(self):
+        return (self.output_directory / self._export_filename()).exists()
+
+    def save(self):
+        with jsonlines.open(self.output_directory / self._export_filename(), mode='w') as writer:
+            if self.sorted:
+                for i in sorted(self.items, key=lambda i: list(i.values())):
+                    writer.write(i)
+            else:
+                for i in self.items:
+                    writer.write(i)
+
+    def get_items(self, filter=None):
+        with jsonlines.open(self.output_directory / self._export_filename()) as reader:
+            for item in reader:
+                if filter and not filter(item):
+                    continue
+
+                yield item
 
 
-class SeleniumLocalHelper(SeleniumHelper):
-    def __init__(self, download_directory, implicit_wait_time, headless=True, **kwargs):
+    def get_sample_items(self, filter=None):
+        sampler = Sampler()
 
-        profile = webdriver.FirefoxProfile()
-        profile.set_preference("browser.download.folderList", 2)
-        profile.set_preference("browser.download.manager.showWhenStarting", False)
-        profile.set_preference("browser.download.dir", download_directory)
-        profile.set_preference("browser.helperApps.neverAsk.saveToDisk", "application/zip")
+        with jsonlines.open(self.output_directory / self._export_filename()) as reader:
+            n = 0
 
-        options = FirefoxOptions()
-        if headless:
-            options.add_argument("--headless")
+            for item in reader:
+                if filter and not filter(item):
+                    continue
 
-        driver = webdriver.Firefox(options=options, firefox_profile=profile)
-        driver.implicitly_wait(implicit_wait_time)
-
-        super().__init__(
-            driver=driver,
-            download_directory=download_directory,
-            **kwargs,
-        )
-
-    def close(self):
-        self.driver.close()
+                if sampler.is_sampling_pick(n):
+                    yield item
+                    
+                n += 1
